5976e77b4f1bc17d89a995042537c3987fdc39f6
Erick Peirson <erick.peirson@asu.edu>
2017-06-23 10:17:00 -0400

    ARXIVDEV-4067 implemented notification consumer

--- reflink/notification/consumer.py
+++ reflink/notification/consumer.py
@@ -1,4 +1,5 @@
 """
+This script is called by the KCL MultiLangDaemon to process Kinesis streams.
 
 http://docs.aws.amazon.com/streams/latest/dev/kinesis-record-processor-implementation-app-py.html
 https://github.com/awslabs/amazon-kinesis-client-python/blob/master/samples/sample_kclpy_app.py
@@ -8,22 +9,37 @@ import sys
 import time
 import logging
 import os
+import json
+from reflink.tasks import orchestrate
 
 # TODO: make this configurable.
 logging.basicConfig(filename=__name__,
                     format='%(asctime)s %(levelname)s: %(message)s',
                     level=logging.DEBUG)
 
+import amazon_kclpy
 from amazon_kclpy import kcl
 from amazon_kclpy.v2 import processor
-from amazon_kclpy.messages import ProcessRecordsInput
+from amazon_kclpy.messages import ProcessRecordsInput, ShutdownInput
 
 
-class RecordProcessor(processor.RecordProcessor):
+class RecordProcessor(processor.RecordProcessorBase):
+    """
+    Processes records received by the Kinesis consumer.
+
+    This class is instantiated when the containing script is run by the
+    MultiLangDaemon. The underlying KCL process handles threading, offset
+    tracking, etc. The KCL guarantees that our :class:`.RecordProcessor` will
+    see each record *at least* once; the checkpoint mechanism gives us a way to
+    further guarantee that we only process each record a single time.
+
+    See the ``consumer.properties`` file for configuration details, including
+    streams.
+    """
     def __init__(self):
         self._SLEEP_SECONDS = 5
         self._CHECKPOINT_RETRIES = 5
-        self._CHECKPOINT_FREQ_SECONDS = 60
+        self._CHECKPOINT_FREQ = 60
         self._largest_seq = (None, None)
         self._largest_sub_seq = None
         self._last_checkpoint_time = None
@@ -36,7 +52,7 @@ class RecordProcessor(processor.RecordProcessor):
         self._last_checkpoint_time = time.time()
 
     def checkpoint(self, checkpointer: amazon_kclpy.kcl.Checkpointer,
-                   sequence_number: str = None,
+                   sequence_number: bytes = None,
                    sub_sequence_number: int = None) -> None:
         """
         Checkpoints with retries on retryable exceptions.
@@ -59,47 +75,74 @@ class RecordProcessor(processor.RecordProcessor):
                     #  dependencies is is over burdened, e.g. too many dynamo
                     #  writes. We will sleep temporarily to let it recover.
                     if self._CHECKPOINT_RETRIES - 1 == n:
-                        sys.stderr.write('Failed to checkpoint after {n} attempts, giving up.\n'.format(n=n))
+                        logging.error("Failed to checkpoint after %i attempts,"
+                                      " giving up." % n)
                         return
                     else:
-                        logging.info('Was throttled while checkpointing, will attempt again in {s} seconds'
-                              .format(s=self._SLEEP_SECONDS))
+                        logging.info("Was throttled while checkpointing, will"
+                                     " attempt again in %i seconds"
+                                     % self._SLEEP_SECONDS)
                 elif 'InvalidStateException' == e.value:
-                    sys.stderr.write('MultiLangDaemon reported an invalid state while checkpointing.\n')
+                    logging.error("MultiLangDaemon reported an invalid state"
+                                  " while checkpointing.")
                 else:  # Some other error
-                    sys.stderr.write('Encountered an error while checkpointing, error was {e}.\n'.format(e=e))
+                    logging.error("Encountered an error while checkpointing,"
+                                  " error was %s" % e)
             time.sleep(self._SLEEP_SECONDS)
 
-    def process_record(self, data: str, partition_key: str,
+    def process_record(self, data: bytes, partition_key: bytes,
                        sequence_number: int, sub_sequence_number: int) -> None:
         """
         Called for each record that is passed to process_records.
+
+        Parameters
+        ----------
+        data : bytes
+        partition_key : bytes
+        sequence_number : int
+        sub_sequence_number : int
         """
 
+        try:
+            deserialized = json.loads(data.decode('utf-8'))
+        except Exception as e:
+            logging.error("Error while deserializing data: %s" % e)
+            logging.error("Data payload: %s" % data)
 
-        return
+        try:
+            orchestrate.process_document(deserialized.get('document_id'))
+        except Exception as e:
+            logging.error("Error while processing document: %s" % e)
+            logging.error("Data payload: %s" % data)
 
     def should_update_sequence(self, sequence_number: int,
                                sub_sequence_number: int) -> bool:
         """
         Determines whether a new larger sequence number is available
+
+        Parameters
+        ----------
+        sequence_number : int
+        sub_sequence_number : int
+
+        Returns
+        -------
+        bool
         """
-        return any([
-            self._largest_seq == (None, None),
-            sequence_number > self._largest_seq[0],
-            all([
-                sequence_number == self._largest_seq[0],
-                sub_sequence_number > self._largest_seq[1]
-            ])
-         ])
+        return self._largest_seq == (None, None) or \
+               sequence_number > self._largest_seq[0] or \
+               (sequence_number == self._largest_seq[0] and \
+                sub_sequence_number > self._largest_seq[1])
 
     def process_records(self, records: ProcessRecordsInput) -> None:
         """
         Called by a KCLProcess with a list of records to be processed and a
         checkpointer which accepts sequence numbers from the records to
-        indicate where in the stream to checkpoint.
-        :param  process_records_input: the records, and metadata about the
-            records.
+        indicate where in the byteseam to checkpoint.
+
+        Parameters
+        ----------
+        records : :class:`amazon_kclpy.messages.ProcessRecordsInput`
         """
         try:
             for record in records.records:
@@ -111,23 +154,26 @@ class RecordProcessor(processor.RecordProcessor):
                 if self.should_update_sequence(seq, sub_seq):
                     self._largest_seq = (seq, sub_seq)
 
-            # Checkpoints every self._CHECKPOINT_FREQ_SECONDS seconds
-            if time.time() - self._last_checkpoint_time > self._CHECKPOINT_FREQ_SECONDS:
-                self.checkpoint(records.checkpointer, str(self._largest_seq[0]),
+            # Checkpoints every self._CHECKPOINT_FREQ seconds
+            if time.time() - self._last_checkpoint_time > self._CHECKPOINT_FREQ:
+                self.checkpoint(records.checkpointer,
+                                bytes(self._largest_seq[0]),
                                 self._largest_seq[1])
                 self._last_checkpoint_time = time.time()
 
         except Exception as e:
-            logging.info("Encountered an exception while processing records."
-                         " Exception was %s" % e)
+            logging.error("Encountered an exception while processing records."
+                          " Exception was %s" % e)
 
-    def shutdown(self, shutdown: amazon_kclpy.messages.ShutdownInput)-> None:
+    def shutdown(self, shutdown: ShutdownInput) -> None:
         """
         Called by a KCLProcess instance to indicate that this record processor
         should shutdown. After this is called, there will be no more calls to
-        any other methods of this record processor. As part of the shutdown
-        process you must inspect
+        any other methods of this record processor.
 
+        Parameters
+        ----------
+        shutdown : :class:`amazon_kclpy.messages.ShutdownInput`
         """
         try:
             if shutdown.reason == 'TERMINATE':
@@ -141,8 +187,9 @@ class RecordProcessor(processor.RecordProcessor):
             else: # reason == 'ZOMBIE'
                 # **ATTEMPTING TO CHECKPOINT ONCE A LEASE IS LOST WILL FAIL**
                 logging.info("Shutting down due to failover. Won't checkpoint.")
-        except:
-            pass
+        except Exception as e:
+            logging.error("Encountered exception while shutting down: %s" % e)
+
 
 if __name__ == "__main__":
     kcl_process = kcl.KCLProcess(RecordProcessor())
--- reflink/tasks/orchestrate.py
+++ reflink/tasks/orchestrate.py
@@ -2,6 +2,7 @@ from celery.exceptions import TaskError
 
 
 def process_document(document_id):
+    print ("!!", document_id)
     try:
         task_id = None  # TODO: here is where we kick off the processing chain.
     except TaskError as e:
