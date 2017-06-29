4f788aa1d63069cedbdc1dce38faca447af3bb24
brp53 <brp53@dhcp-gs-5538.redrover.cornell.edu>
2017-06-29 15:35:58 -0400

    ARXIVDEV-4081 refactored to run processing steps serially

--- reflink/process/extract/__init__.py
+++ reflink/process/extract/__init__.py
@@ -11,3 +11,25 @@ Extractor:
 - Extracts reference lines and structured reference metadata;
 - Returns reference lines and metadata.
 """
+
+from reflink.process.extract import cermine
+
+
+def extract_cermine(pdf_path: str) -> list:
+    """
+    Extract the list of reference metadata from a PDF using CERMINE.
+
+    Parameters
+    ----------
+    pdf_path : str
+        Location of an arXiv PDF on the filesystem.
+
+    Returns
+    -------
+    list
+        Extracted reference metadata.
+    """
+    return cermine.extract_references(pdf_path)
+
+
+extract = extract_cermine
