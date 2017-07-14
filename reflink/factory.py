7061511e26c703d712f39eac662b72260552c666
erickpeirson <brp53@cornell.edu>
2017-07-14 15:53:12 -0400

    configuration, logging, etc

--- reflink/factory.py
+++ reflink/factory.py
@@ -1,11 +1,15 @@
 """Application factory for reflink service components."""
 
 from flask import Flask
+import logging
 
 
 def create_web_app():
     """Initialize an instance of the web application."""
     from reflink.web.routes import rest
+    logging.getLogger('boto3').setLevel(logging.WARNING)
+    logging.getLogger('botocore').setLevel(logging.WARNING)
+
     app = Flask('reflink')
     app.config.from_pyfile('config.py')
     app.register_blueprint(rest.blueprint)
@@ -15,4 +19,6 @@ def create_web_app():
 def create_process_app():
     """Initialize an instance of the processing application."""
     from reflink.celery import app
+    logging.getLogger('boto3').setLevel(logging.WARNING)
+    logging.getLogger('botocore').setLevel(logging.WARNING)
     return app
