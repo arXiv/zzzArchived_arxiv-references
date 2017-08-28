7599dbfd94e583fadddb02d2535680d6ec65a14b
erickpeirson <brp53@cornell.edu>
2017-08-28 15:51:03 -0400

    more deployment tweaks

--- deploy/installDependencies.sh
+++ deploy/installDependencies.sh
@@ -2,4 +2,4 @@
 
 virtualenv --python=/usr/bin/python3 /opt/reflink/venv
 source /opt/reflink/venv/bin/activate
-pip install -r /opt/reflink/arxiv-reflink/requirements.txt
+pip install -r /opt/reflink/arxiv-reflink/requirements_backend.txt
